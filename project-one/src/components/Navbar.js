442fc85ff77e4ee6df119ecb26d1fb193d04db7c
Christina <christina.phillips9838@gmail.com>
2023-02-21 21:56:23 -0500

    Added all the different files for the pages tha t will be used and a file for the navbar

---
+++ project-one/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css';
+import { Link } from "react-router-dom";
+
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
